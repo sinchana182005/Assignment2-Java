067d8322555bfe07dd2ee28d53f71d85f33c16c2
sinchana182005 <msinchana681@gmail.com>
2026-03-09 21:18:32 +0530

    initial commit

---
+++ src/Assignment/StableExample.java
@@ -0,0 +1,11 @@
+package Assignment;
+
+public class StableExample {
+
+
+        public static void main(String[] args) {
+            System.out.println("Stable Sort Example: Merge Sort, Insertion Sort");
+            System.out.println("Unstable Sort Example: Quick Sort, Selection Sort");
+        }
+    }
+
